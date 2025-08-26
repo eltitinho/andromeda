527e999e1715f75be3ae640ca67efbff47e22701
kristian <kristian.harge@yahoo.com>
2025-08-26 23:49:36 +0200

    Add support for tracking

--- tracking.py
+++ tracking.py
@@ -1,17 +1,29 @@
 # app.py
-from flask import Flask, request, render_template, send_file
-from reportlab.lib.pagesizes import letter
-from reportlab.pdfgen import canvas
-from PyPDF2 import PdfWriter, PdfReader
-import io
-from PIL import Image
-from datetime import datetime
+from flask import Flask, render_template, request, redirect, url_for
+import sqlite3
 
 tracking = Flask(__name__)
 
-@tracking.route('/')
-def tack():
-    return render_template('tracking.html')
+def get_db_connection():
+    conn = sqlite3.connect('tracking.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+@tracking.route('/', methods=['GET', 'POST'])
+def track_number():
+    print("Request method:", request.method)
+    if request.method == 'POST':
+        tracking_number = request.form['tracking_number']
+        return redirect(url_for('track', tracking_number=tracking_number))
+    return render_template('tracking_number.html')
+
+@tracking.route('/track/<tracking_number>')
+def track(tracking_number):
+    conn = get_db_connection()
+    status = conn.execute('SELECT * FROM tracking WHERE tracking_number = ?',
+                          (tracking_number,)).fetchone()
+    conn.close()
+    return render_template('tracking.html', status=status)
 
 if __name__ == '__main__':
     tracking.run(debug=True)
